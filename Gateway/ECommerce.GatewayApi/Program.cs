da40d4caca0ec81e2c3f118085e560a520f885e1
Münevver Orman <munevverorman1@gmail.com>
2026-03-18 18:34:08 +0300

    #

--- Gateway/ECommerce.GatewayApi/Program.cs
+++ Gateway/ECommerce.GatewayApi/Program.cs
@@ -1,17 +1,16 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
-var ocelotConfiguration= new ConfigurationBuilder().AddJsonFile("ocelotconfig.json").Build();
+var ocelotConfiguration = new ConfigurationBuilder().AddJsonFile("ocelotconfig.json").Build();
 
-builder.Services.AddOcelot();
-
+builder.Services.AddOcelot(ocelotConfiguration);
 
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
 
 
 await app.UseOcelot();
